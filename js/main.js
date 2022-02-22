8aeed62c12c6780f30520e5317e3dd464d509105
afmssrahi <afmssrahi@gmail.com>
2022-02-22 09:51:33 +0600

    hero-sectiom complete

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger-menu').addEventListener('click', () => {
+	const mainMenu = document.getElementById('main-menu');
+
+	mainMenu.classList.toggle('show-main-menu');
+});
