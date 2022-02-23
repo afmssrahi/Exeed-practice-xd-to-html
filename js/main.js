2882390597b9ea5cbc29dddd12ac27dd9e181521
afmssrahi <afmssrahi@gmail.com>
2022-02-23 16:09:35 +0600

    menu toggle complete by JS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-document.getElementById('hamburger-menu').addEventListener('click', () => {
-	const mainMenu = document.getElementById('main-menu');
+const mainMenu = document.getElementById('main-menu');
+const navBar = document.getElementById('navbar');
 
+// fixing auto toggle problem
+let mainMenuToggle = true;
+
+//
+document.getElementById('hamburger-menu').addEventListener('click', () => {
 	mainMenu.classList.toggle('show-main-menu');
+	navBar.classList.toggle('navbar-background');
+	mainMenuToggle = !mainMenuToggle;
+});
+
+window.addEventListener('scroll', () => {
+	if (
+		document.body.scrollTop > 150 ||
+		document.documentElement.scrollTop > 150
+	) {
+		if (mainMenuToggle) {
+			mainMenu.classList.add('show-main-menu');
+			navBar.classList.add('navbar-background');
+		}
+	} else {
+		if (mainMenuToggle) {
+			mainMenu.classList.remove('show-main-menu');
+			navBar.classList.remove('navbar-background');
+		}
+	}
 });
